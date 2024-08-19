5e7024f9e0df67baff869f6af3508a7f5bb2a33b
Bunyawat Srisompong <bunyawat@blockfint.com>
2024-08-19 20:38:58 +0700

    Add JWT authentication middleware
    Introduce middleware for verifying JWT tokens from cookies and validating user claims. This provides an essential layer of security by ensuring only authenticated requests have access to protected routes, enhancing resilience against unauthorized access.

---
+++ internal/auth/middleware.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/bsrisompong/google-oauth-go-server/pkg/utils"
+)
+
+type contextKey string
+
+const userContextKey = contextKey("user")
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("tokens")
+		if err != nil {
+			utils.ErrorResponse(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
+		claims, err := ValidateJWT(cookie.Value)
+		if err != nil {
+			utils.ErrorResponse(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, userContextKey, claims)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
