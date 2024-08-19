8af9d45454c9aab0eb1e1068b5345f3620a74d1d
Bunyawat Srisompong <bunyawat@blockfint.com>
2024-08-19 18:58:41 +0700

    Add User model and database initialization
    Introduced a User model within the internal models package to define the structure for user data. Added a new database package with an `InitDB` function for setting up and verifying a PostgreSQL database connection. These changes lay the groundwork for user management and database interactions.

---
+++ internal/models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID            string
+	Email         string
+	Name          string
+	Picture       string
+	VerifiedEmail bool
+}
---
+++ pkg/db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq" // PostgreSQL driver
+)
+
+var DB *sql.DB
+
+func InitDB(connStr string) {
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := DB.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+}
