381fba9944cea2b9b71b8b9dbf625d279a280b46
Bunyawat Srisompong <bunyawat@blockfint.com>
2024-08-19 21:08:40 +0700

    Add Google OAuth integration and auth handler
    Refactored Google OAuth configuration to a new package and enhanced it to also fetch user profile info. Introduced a new auth handler to process Google auth requests, exchange auth codes for tokens, and manage user sessions. Incorporates new routes for Google authentication and user logout, and implements JWT-based user sessions. Addresses issues with user creation and retrieval in the database, ensuring new users are added correctly during Google sign-in;

---
+++ internal/google/google.go
@@ -0,0 +1,51 @@
+package google
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/bsrisompong/google-oauth-go-server/internal/config"
+	"github.com/bsrisompong/google-oauth-go-server/internal/models"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var googleOauthConfig *oauth2.Config
+
+func InitGoogleOAuth() {
+	googleOauthConfig = &oauth2.Config{
+		ClientID:     config.GoogleClientId,
+		ClientSecret: config.GoogleClientSecret,
+		RedirectURL:  config.GoogleRedirectURL,
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+		// Endpoint:     oauth2.Endpoint{TokenURL: "https://accounts.google.com/o/oauth2/token"},
+		Endpoint: google.Endpoint,
+	}
+}
+
+func ExchangeCodeForToken(code string) (*oauth2.Token, error) {
+	return googleOauthConfig.Exchange(context.TODO(), code)
+}
+
+func GetUserInfo(token *oauth2.Token) (*models.User, error) {
+	client := oauth2.NewClient(context.TODO(), oauth2.StaticTokenSource(token))
+
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user info: received status code %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get user info: received status code %d", resp.StatusCode)
+	}
+
+	var user models.User
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		return nil, fmt.Errorf("failed to decode user info: %v", err)
+	}
+
+	return &user, nil
+}
---
+++ internal/handlers/auth_handler.go
@@ -0,0 +1,118 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/bsrisompong/google-oauth-go-server/internal/auth"
+	"github.com/bsrisompong/google-oauth-go-server/internal/google"
+	"github.com/bsrisompong/google-oauth-go-server/internal/models"
+	"github.com/bsrisompong/google-oauth-go-server/pkg/db"
+	"github.com/bsrisompong/google-oauth-go-server/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+func RegisterAuthRoutes(r *mux.Router) {
+	r.HandleFunc("/auth/google", http.HandlerFunc(exchangeToken)).Methods("POST")
+	r.HandleFunc("/auth/logout", logout).Methods("POST")
+
+}
+
+func exchangeToken(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Code string `json:"code"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		utils.ErrorResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	token, err := google.ExchangeCodeForToken(req.Code)
+	if err != nil {
+		utils.ErrorResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	userInfo, err := google.GetUserInfo(token)
+	if err != nil {
+		utils.ErrorResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user, err := getUserFromDB(userInfo.Email)
+	if err != nil && err != sql.ErrNoRows {
+		utils.ErrorResponse(w, "Database error", http.StatusBadRequest)
+		return
+	}
+
+	if user == nil {
+		user = &models.User{
+			ID:            userInfo.ID,
+			Email:         userInfo.Email,
+			Name:          userInfo.Name,
+			Picture:       userInfo.Picture,
+			VerifiedEmail: userInfo.VerifiedEmail,
+		}
+		err = createUserInDB(user)
+		if err != nil {
+			utils.ErrorResponse(w, "Failed to create user", http.StatusBadRequest)
+			return
+		}
+	}
+
+	jwtToken, err := auth.CreateJWT(user)
+	if err != nil {
+		utils.ErrorResponse(w, "Failed to create JWt", http.StatusBadRequest)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Value:    jwtToken,
+		Expires:  time.Now().Add(24 * time.Hour),
+		Path:     "/", //
+		HttpOnly: true,
+		Secure:   false, // true for https
+		SameSite: http.SameSiteNoneMode,
+	})
+
+	utils.JSONResponse(w, map[string]string{
+		"status": "success",
+	}, http.StatusOK)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Value:    "",
+		Expires:  time.Now(),
+		Path:     "/", //
+		HttpOnly: true,
+		Secure:   false, // true for https
+		SameSite: http.SameSiteNoneMode,
+	})
+
+	utils.JSONResponse(w, map[string]string{
+		"status": "success",
+	}, http.StatusOK)
+}
+
+func createUserInDB(user *models.User) error {
+	query := "INSERT INTO users (id, email, name, picture, verified_email) VALUES ($1, $2, $3, $4, $5)"
+	_, err := db.DB.Exec(query, user.ID, user.Email, user.Name, user.Picture, user.VerifiedEmail)
+	return err
+}
+
+func getUserFromDB(email string) (*models.User, error) {
+	user := &models.User{}
+	query := "SELECT id, email, name, picture, verified_email FROM users WHERE email =$1"
+	err := db.DB.QueryRow(query, email).Scan(&user.ID, &user.Email, &user.Name, &user.Picture, &user.VerifiedEmail)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
